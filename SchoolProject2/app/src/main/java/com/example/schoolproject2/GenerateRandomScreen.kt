58e55f6041abe2fc887e9e11aa11c39c67ba9d96
James Spencer <jespencer@student.fullsail.edu>
2022-03-15 08:24:34 -0700

    Completely functional database implementation and random functionality.

--- SchoolProject2/app/src/main/java/com/example/schoolproject2/GenerateRandomScreen.kt
+++ SchoolProject2/app/src/main/java/com/example/schoolproject2/GenerateRandomScreen.kt
@@ -1,14 +1,19 @@
 package com.example.schoolproject2
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.tasks.Task
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_generate_random_screen.*
-import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.navView
 import kotlin.random.Random
 import kotlin.random.nextInt
@@ -17,33 +22,93 @@ class GenerateRandomScreen : AppCompatActivity()
 {
     lateinit var toggle: ActionBarDrawerToggle
 
-    private val nameList: MutableList<String> = mutableListOf("Alex","James", "Sarah", "Clifton", "Emily")
-    private val raceList: MutableList<String> = mutableListOf("Human","Elf", "Orc", "Gnome", "Dwarf", "Dragonborn")
-    private val lifestyleList: MutableList<String> = mutableListOf("Hardworking","Lazy", "Wretched", "Wealthy", "Modest",)
-    private val occupationList: MutableList<String> = mutableListOf("Shop Keeper","Fletcher", "Blacksmith", "Miner", "Alchemist", "Cobbler", "Chef", "Stable Master" )
-    private val alignmentList: MutableList<String> = mutableListOf("Lawful Good","Neutral Good", "Chaotic Good", "Lawful Neutral", "True Neutral", "Chaotic Neutral", "Lawful Evil", "Neutral Evil", "Chaotic Evil")
-    private val bondList: MutableList<String> = mutableListOf("Protector","Loves Family", "Must Be Seen As A Hero", "Would Die For Honor", "Owes An Unpayable Debt")
-    private val flawList: MutableList<String> = mutableListOf("Greedy","Sucker For Pretty Faces", "Gambler", "Doesn't Know When To Stop", "Bites Off More Than They Can Chew")
-    private val idealsList: MutableList<String> = mutableListOf("Faith","Tradition", "Power", "Community", "Respect", "Glory", "Honor", "Nature")
-    private val physicalTraitsList: MutableList<String> = mutableListOf("Scar Above Eye","Giant Hands", "Golden Hair", "Crooked Nose", "Face Tattoos", "Nose Piercing")
+    // Write a message to the database
+    private lateinit var database: FirebaseDatabase
+    private lateinit var dbReference: DatabaseReference
+    private lateinit var dataSnapShot: Task<DataSnapshot>
+    //private val myRef = database.getReference()
+    //private val humanMale = mDatabase.getReference("humanMale")
+    //val physicalTraits = mDatabase.getReference("physicalTraits")
+    //wants to convert to direct property syntax - ignoring for now / functions fine as is //JES
 
-    override fun onCreate(savedInstanceState: Bundle?)
-    {
+    private val nameList: MutableList<String> = mutableListOf()
+    private val raceList: MutableList<String> = mutableListOf("Human","Elf", "Half Elf", "Gnome", "Dwarf", "Dragonborn", "Halfling")
+    private val lifestyleList: MutableList<String> = mutableListOf("Hardworking","Lazy", "Wretched", "Wealthy", "Modest",)
+    private val occupationList: MutableList<String> = mutableListOf()
+    private val alignmentList: MutableList<String> = mutableListOf()
+    private val bondList: MutableList<String> = mutableListOf()
+    private val flawList: MutableList<String> = mutableListOf()
+    private val idealsList: MutableList<String> = mutableListOf()
+    private var physicalTraitsList: MutableList<String> = mutableListOf()
+    //val physicalTraitsList: MutableList<String> = mutableListOf("blah blah", "meow", "woof")
+
+    //private val physicalTraitsDatabase: MutableList<String> = database.getReference()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        //think about getting the dataSnapshot into an array or list to speed production up
+        database = FirebaseDatabase.getInstance()
+        dbReference = database.getReference("/physicalDatabase")
+        dataSnapShot = dbReference.get()
+        //query = dbReference.child("physical")
+        //query.addValueEventListener()
+        //physicalTraitsList = mutableListOf(dataSnapShot.result.children.toString())
+        while ((!dataSnapShot.isComplete)) {
+            Log.e("Task", "Waiting on Physical")
+        }
+        for (item in dataSnapShot.result.children) {
+            physicalTraitsList.add(item.value.toString())
+        }
+        dbReference = database.getReference("/flawDatabase")
+        dataSnapShot = dbReference.get()
+        while((!dataSnapShot.isComplete)){
+            Log.e("Task", "Waiting on Flaws")
+        }
+        for(item in dataSnapShot.result.children){
+            flawList.add(item.value.toString())
+        }
+        dbReference = database.getReference("/occupationDatabase")
+        dataSnapShot = dbReference.get()
+        while((!dataSnapShot.isComplete)){
+            Log.e("Task", "Waiting on Occupation")
+        }
+        for(item in dataSnapShot.result.children){
+            occupationList.add(item.value.toString())
+        }
+        dbReference = database.getReference("/bondDatabase")
+        dataSnapShot = dbReference.get()
+        while((!dataSnapShot.isComplete)){
+            Log.e("Task", "Waiting on Bond")
+        }
+        for(item in dataSnapShot.result.children){
+            bondList.add(item.value.toString())
+        }
+        dbReference = database.getReference("/alignmentDatabase")
+        dataSnapShot = dbReference.get()
+        while((!dataSnapShot.isComplete)){
+            Log.e("Task", "Waiting on Alignment")
+        }
+        for(item in dataSnapShot.result.children){
+            alignmentList.add(item.value.toString())
+        }
+        dbReference = database.getReference("/anyIdeal")
+        dataSnapShot = dbReference.get()
+        while((!dataSnapShot.isComplete)){
+            Log.e("Task", "Waiting on Ideal")
+        }
+        for(item in dataSnapShot.result.children){
+            idealsList.add(item.value.toString())
+        }
+        dbReference = database.getReference("/humanMale")
+        dataSnapShot = dbReference.get()
+        while((!dataSnapShot.isComplete)){
+            Log.e("Task", "Waiting on humanMale")
+        }
+        for(item in dataSnapShot.result.children){
+            nameList.add(item.value.toString())
+        }
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_generate_random_screen)
-
-//        var lifeStyleBtn = findViewById<Button>(R.id.button5)
-//        lifeStyleBtn.setOnClickListener{
-//           var builder = android.app.AlertDialog.Builder(this)
-//            builder.setTitle("Life Style")
-//            builder.setMessage( "Life Style Info Here")
-//            builder.setNeutralButton("OK"){dialoginterface, which->}
-//
-//            var lifeStyleDialog = builder.create()
-//            lifeStyleDialog.setCancelable(false)
-//            lifeStyleDialog.show()
-//        }
-
 
         toggle = ActionBarDrawerToggle(this,genCharDrawerLayout, R.string.open, R.string.close)
         genCharDrawerLayout.addDrawerListener(toggle)
@@ -51,11 +116,10 @@ class GenerateRandomScreen : AppCompatActivity()
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-
-
         val generateBtn = findViewById<Button>(R.id.button2)
         generateBtn.setOnClickListener(View.OnClickListener
         {
+
             val randomAge = Random.nextInt(1..99)
             textView8.text = "$randomAge"
 
@@ -83,9 +147,20 @@ class GenerateRandomScreen : AppCompatActivity()
             val randomIdeals = (0 until(idealsList.size)).random()
             textView39.text = idealsList[randomIdeals]
 
-            val randomPysicalTraits = (0 until(physicalTraitsList.size)).random()
-            textView40.text = physicalTraitsList[randomPysicalTraits]
+            val randomPhysicalTraits = (0 until(physicalTraitsList.size)).random()
+            val physicalInformation: String = physicalTraitsList[randomPhysicalTraits]
+            textView40.text = physicalInformation
 
+            try { //method to attempt to stall the crash of program
+                super.onDestroy()
+            }
+            catch (e: Exception) {
+                e.printStackTrace()
+            }
+            //val randomHumanMale = (0 until(humanMaleList.size)).random()
+//            val randomPhysicalTraits = Random.nextInt(0..98)
+//                //(0 until(physicalTraitsList.size)).random()
+//                textView40.text = physicalTraitsList[randomPhysicalTraits].toString()
         })
 
 
@@ -112,6 +187,23 @@ class GenerateRandomScreen : AppCompatActivity()
             true
         }
 
+        // Read from the database
+//        myRef.addValueEventListener(object: ValueEventListener {
+//
+//            override fun onDataChange(snapshot: DataSnapshot) {
+//                // This method is called once with the initial value and again
+//                // whenever data at this location is updated.
+//                val value = snapshot.getValue<String>()
+//                if (value != null) {
+//                    Log.d(TAG, value)
+//                }
+//            }
+//
+//            override fun onCancelled(error: DatabaseError) {
+//                Log.w(TAG, "Failed to read value.", error.toException())
+//            }
+//
+//        })
     }
 
 
@@ -126,6 +218,4 @@ class GenerateRandomScreen : AppCompatActivity()
         }
         return super.onOptionsItemSelected(item)
     }
-
-
 }
